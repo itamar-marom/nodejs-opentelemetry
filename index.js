77ffd87b67355c2fba1285a625437405449fd600
itamar-marom <marom.itamar@gmail.com>
2022-01-06 20:27:12 +0200

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+// const { countAllRequests } = require("./monitoring");
+const PORT = process.env.PORT || "8080";
+const app = express();
+// app.use(countAllRequests());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(parseInt(PORT, 10), () => {
+  console.log(`Listening for requests on http://localhost:${PORT}`);
+});
